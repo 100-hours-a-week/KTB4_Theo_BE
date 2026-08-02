00559fac8285760538f720149299fa989e21be0f
jaksfj <myki011122@gmail.com>
2026-08-02 14:12:54 +0900

    test: add MySQL test profile

--- src/test/java/com/theo/community_api/notification/NotificationRepositoryTest.java
+++ src/test/java/com/theo/community_api/notification/NotificationRepositoryTest.java
@@ -12,6 +12,7 @@ import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.PageRequest;
@@ -22,6 +23,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class NotificationRepositoryTest {
 
     @Autowired
